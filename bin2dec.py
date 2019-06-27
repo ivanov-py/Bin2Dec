d72b92560c4a3f4411ff69343a8dd9eb1b6486a8
Ilya <iiv@checku.co>
2019-06-28 00:36:09 +0300

    edited optimization

--- bin2dec.py
+++ bin2dec.py
@@ -1,35 +1,25 @@
 BIN_START_SUBSTRING = '0b'
+CHAR_ONE = '1'
+CHAR_ZERO = '0'
 
 
 def to_dec(bin_: str) -> int:
 
-    _check_is_bin(bin_)
+    if not bin_.startswith(BIN_START_SUBSTRING):
+        raise TypeError('bin value must starts with `{}`'.format(BIN_START_SUBSTRING))
 
     chars = bin_[2:]
 
-    _check_contain_only_1_and_0(chars)
-    _check_has_chars(chars)
+    if not chars:
+        raise ValueError('No chars for converting')
 
     result = 0
 
     for ind, c in enumerate(reversed(chars)):
-        if c == '1':
+        if c == CHAR_ONE:
             to_add = 2 ** ind
             result += to_add
+        elif c != CHAR_ZERO:
+            raise ValueError('bin string must contain only `1` and `0` chars')
 
     return result
-
-
-def _check_is_bin(s: str):
-    if not s.startswith(BIN_START_SUBSTRING):
-        raise TypeError('bin value must starts with `{}`'.format(BIN_START_SUBSTRING))
-
-
-def _check_contain_only_1_and_0(s: str):
-    if not all(map(lambda x: x in ['0', '1'], s)):
-        raise ValueError('bin string must contain only `1` and `0` chars')
-
-
-def _check_has_chars(s: str):
-    if not s:
-        raise ValueError('No chars for converting')
